8bed73014bf97b02181963047cd8cd3d152ed53a
Tomasz Chojnacki <tomaszchojnacki2001@gmail.com>
2025-04-02 00:54:56 +0200

    Move the dataset

--- tcp-framework/main.py
+++ tcp-framework/main.py
@@ -11,9 +11,9 @@ from tcp_framework import (
 )
 
 little_proxy = TcpDataset(
-    runs_path="../experiments/new-approach/adamfisk@LittleProxy.csv",
-    repo_path="../experiments/new-approach/LittleProxy",
-    run_to_commit_path="../experiments/new-approach/tr_all_built_commits.csv",
+    runs_path="./datasets/adamfisk@LittleProxy.csv",
+    repo_path="./datasets/LittleProxy",
+    run_to_commit_path="./datasets/tr_all_built_commits.csv",
 )
 
 approaches: list[TcpApproach] = [
