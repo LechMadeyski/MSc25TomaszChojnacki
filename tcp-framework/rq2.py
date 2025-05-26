4ba3014146e2b4a6d1e28f51f27bec0b5cceb09a
Tomasz Chojnacki <tomaszchojnacki2001@gmail.com>
2025-05-26 22:28:52 +0200

    Gather partial RQ2.3 results

--- tcp-framework/rq2.py
+++ tcp-framework/rq2.py
@@ -3,7 +3,16 @@
 from pathlib import Path
 
 from tcp_framework import Dataset, dump_results, evaluate
-from tcp_framework.approaches import BordaMixedOrder, ExeTimeOrder, FoldFailsOrder, RandomMixedOrder, SchulzeMixedOrder
+from tcp_framework.approaches import (
+    BordaMixedOrder,
+    CodeDistBreakedOrder,
+    CodeDistOrder,
+    ExeTimeOrder,
+    FoldFailsOrder,
+    GenericBreakedOrder,
+    RandomMixedOrder,
+    SchulzeMixedOrder,
+)
 
 if __name__ == "__main__":
     cycle_map = Dataset.preload_cycle_map(Path("./datasets/travistorrent_8_2_2017.csv"), debug=True)
@@ -26,22 +35,22 @@ if __name__ == "__main__":
     ]
 
     # (RQ2.1) Can mixers beat their sub-approaches in terms of effectiveness?
-    print("=== rq21 ===")
-    approaches = [
-        FoldFailsOrder(),
-        ExeTimeOrder(),
-        BordaMixedOrder([FoldFailsOrder(), ExeTimeOrder()], [0.6, 0.4]),
-        RandomMixedOrder([FoldFailsOrder(), ExeTimeOrder()], [0.6, 0.4]),
-        SchulzeMixedOrder([FoldFailsOrder(), ExeTimeOrder()], [0.6, 0.4]),
-    ]
-    for dataset in datasets:
-        results = evaluate(
-            approaches,
-            dataset,
-            ["rAPFDc"],
-            debug=1,
-        )
-        dump_results(Path("./out/rq21.json"), dataset.name, [r.r_apfd_c_list for r in results])
+    # print("=== rq21 ===")
+    # approaches = [
+    #     FoldFailsOrder(),
+    #     ExeTimeOrder(),
+    #     BordaMixedOrder([FoldFailsOrder(), ExeTimeOrder()], [0.6, 0.4]),
+    #     RandomMixedOrder([FoldFailsOrder(), ExeTimeOrder()], [0.6, 0.4]),
+    #     SchulzeMixedOrder([FoldFailsOrder(), ExeTimeOrder()], [0.6, 0.4]),
+    # ]
+    # for dataset in datasets:
+    #     results = evaluate(
+    #         approaches,
+    #         dataset,
+    #         ["rAPFDc"],
+    #         debug=1,
+    #     )
+    #     dump_results(Path("./out/rq21.json"), dataset.name, [r.r_apfd_c_list for r in results])
 
     # (RQ2.2) Can interpolators beat their sub-approaches in terms of effectiveness?
     # print(f"=== {filename} ===")
@@ -50,19 +59,25 @@ if __name__ == "__main__":
     #     results = evaluate(
     #         approaches,
     #         dataset,
-    #         ["rAPFD"],
+    #         ["rAPFDc"],
     #         debug=1,
     #     )
-    #     dump_results(Path(f"./out/{filename}.json"), dataset.name, [r.r_apfd_list for r in results])
+    #     dump_results(Path(f"./out/{filename}.json"), dataset.name, [r.r_apfd_c_list for r in results])
 
     # (RQ2.3) Can tiebreakers beat their base approach in terms of effectiveness?
-    # print(f"=== {filename} ===")
-    # approaches = [factory(option) for option in options]
-    # for dataset in datasets:
-    #     results = evaluate(
-    #         approaches,
-    #         dataset,
-    #         ["rAPFD"],
-    #         debug=1,
-    #     )
-    #     dump_results(Path(f"./out/{filename}.json"), dataset.name, [r.r_apfd_list for r in results])
+    print("=== rq23 ===")
+    approaches = [
+        FoldFailsOrder("total", seed=None),
+        ExeTimeOrder(),
+        CodeDistOrder(),
+        GenericBreakedOrder(target=FoldFailsOrder("total", seed=None), breaker=ExeTimeOrder()),
+        CodeDistBreakedOrder(target=FoldFailsOrder("total", seed=None)),
+    ]
+    for dataset in datasets:
+        results = evaluate(
+            approaches,
+            dataset,
+            ["rAPFDc"],
+            debug=1,
+        )
+        dump_results(Path("./out/rq23.json"), dataset.name, [r.r_apfd_c_list for r in results])
