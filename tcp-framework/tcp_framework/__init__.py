5998680181ff7d385816eeb3e69ac7d49d273432
Tomasz Chojnacki <tomaszchojnacki2001@gmail.com>
2025-05-30 18:39:31 +0200

    Remove metric_boxplot

--- tcp-framework/tcp_framework/__init__.py
+++ tcp-framework/tcp_framework/__init__.py
@@ -1,5 +1,5 @@
-__version__ = "0.1.0"
+__version__ = "1.0.0"
 
 from .dataset import Dataset as Dataset
 from .evaluate import evaluate as evaluate, evaluate_one as evaluate_one
-from .stats import dump_results as dump_results, metric_boxplot as metric_boxplot
+from .stats import dump_results as dump_results
--- tcp-framework/tcp_framework/stats.py
+++ tcp-framework/tcp_framework/stats.py
@@ -2,30 +2,6 @@ import json
 from collections.abc import Sequence
 from pathlib import Path
 
-import matplotlib.pyplot as plt
-
-
-def metric_boxplot(
-    file_path: Path,
-    values: Sequence[Sequence[float]],
-    *,
-    title: str | None = None,
-    labels: Sequence[str] | None = None,
-) -> None:
-    if labels is None:
-        labels = [f"A{i + 1}" for i in range(len(values))]
-    assert len(values) == len(labels), "values and labels must have the same length"
-
-    plt.clf()
-    plt.rcParams.update({"font.size": 12})
-    if title is not None:
-        plt.title(title)
-    plt.xlabel("Approach")
-    plt.ylabel("Value")
-    plt.ylim(bottom=0.0, top=1.0)
-    plt.boxplot(values, tick_labels=labels)
-    plt.savefig(file_path)
-
 
 def dump_results(file_path: Path, key: str, results: Sequence[Sequence[float]]) -> None:
     content: dict[str, Sequence[Sequence[float]]] = {}
